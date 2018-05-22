99c30ef29effcb310d49a3b61e68d1898c6c02f9
Timothy Hoang <timh1203@gmail.com>
2018-05-22 11:46:24 -0400

    Add draft of resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,19 +1,176 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import { Section, H1, P } from '../assets/Matrix'
+import { Section, H1, H2, P } from '../assets/Matrix'
 
 const ResourcesPage = () => (
   <Section>
     <H1>Resources</H1>
+
+    <H2>Travel Resources</H2>
+    <P>These are the services I use when I go travel and they have helped me in many difference ways.
+    </P>
+    <P>
+      <Link to="#">Chase Sapphire Reserve</Link> – 
+    </P>
+    <P>
+      <Link to="#">Charles Schwab Debit Card</Link> – 
+    </P>
+    <P>
+      <Link to="#">World Nomads</Link> – 
+    </P>
+    <P>
+      <Link to="#">Charles Schwab Debit Card</Link> – 
+    </P>
+    <P>
+      <Link to="#">AirBnB</Link> – 
+    </P>
+    <P>
+      <Link to="#">Uber</Link> – 
+    </P>
+
+    <H2>My Computer Tools</H2>
+    <P>
+    </P>
+    <P>
+      <Link to="#">Macbook Pro 2015 (15'-inch)</Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B077GPYD96/ref=oh_aui_detailpage_o03_s01?ie=UTF8&psc=1">
+        iLeadon Macbook Pro 15 Inch Case with Retina Display 2012-2015 Release Model A1398 Rubberized Hard Shell Cover+Keyboard Cover For MacBook Pro 15" Retina Non CD ROM, Nebula Map
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/Roost-Laptop-Stand-Adjustable-Portable/dp/B01C9KG8IG/ref=sr_1_4?ie=UTF8&qid=1527001259&sr=8-4&keywords=roost+stand">
+        Roost Laptop Stand
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B00PIMIUPW/ref=oh_aui_detailpage_o03_s01?ie=UTF8&psc=1">
+        Anker Ultra Compact Slim Profile Wireless Bluetooth Keyboard with Rechargeable Battery
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B01LF37K80/ref=oh_aui_detailpage_o01_s00?ie=UTF8&psc=1">
+        Logitech M720 Triathalon Multi-Device Wireless Mouse
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/Wireless-ENACFIRE-Bluetooth-Playtime-Headphones/dp/B078WP9M4F/ref=sr_1_3?s=electronics&ie=UTF8&qid=1527001311&sr=1-3&keywords=enacfire+e18+bluetooth+earbuds">
+        EnacFire E18 Bluetooth Earbuds
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B003EM800S/ref=oh_aui_detailpage_o01_s01?ie=UTF8&psc=1">
+        Panasonic ErgoFit In-Ear Earbud Headphones RP-HJE120-S (Silver) Dynamic Crystal Clear Sound, Ergonomic Comfort-Fit
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B01LQQH86A/ref=oh_aui_detailpage_o02_s00?ie=UTF8&psc=1">
+        WD 4TB Black My Passport  Portable External Hard Drive - USB 3.0
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B00ATZJ5YS/ref=oh_aui_detailpage_o03_s00?ie=UTF8&psc=1">
+        Belkin SurgePlus USB Swivel Surge Protector and Charger (Power strip with 3 AC Outlets, 2 USB Ports 2.1 AMP/10 Watt) and rotating plug
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B0114C1TM0/ref=oh_aui_detailpage_o02_s00?ie=UTF8&psc=1">
+        TSA Luggage Locks (2 Pack) - 4 Digit Combination Steel Padlocks
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/Under-Armour-Hustle-Backpack-Black/dp/B077H6F887/ref=sr_1_2?s=pc&ie=UTF8&qid=1527002564&sr=8-2&keywords=Under%2BArmour%2BTeam%2BHustle%2B3.0%2BBackpack&th=1&psc=1">
+        Under Armour UA Hustle 3.0 Backpack
+      </Link> – 
+    </P>
+
+    <H2>My Phone Tools</H2>
+    <P>
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/Samsung-Galaxy-S4-Touchscreen-International/dp/B00S3FEOAG/ref=sr_1_1?s=wireless&ie=UTF8&qid=1527001874&sr=1-1&keywords=samsung+galaxy+s4">
+        Samsung Galaxy S4 SGH-I337 Unlocked GSM Smartphone with 13 MP Camera, Touchscreen and 16 GB Storage, Black (International Version)
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B003EM800S/ref=oh_aui_detailpage_o01_s01?ie=UTF8&psc=1">
+        Anker 7800mAh Extended Battery Combo for Samsung Galaxy S4, I9500, I9505, Galaxy S4 LTE I9506 Galaxy J M919 (T-Mobile) I545 (Verizon) I337 (AT&T) L720 (Sprint) [Not for Galaxy S4 Active] - TPU back cover included
+      </Link> – 
+    </P>
+
+    <H2>My Camera Tools</H2>
+    <P>
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/GoPro-CHDHX-502-HERO5-Black/dp/B01M14ATO0/ref=sr_1_3?s=electronics&ie=UTF8&qid=1527001969&sr=1-3&keywords=gopro+5+camera">
+        GoPro HERO5 Black
+      </Link> –
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B00GTZJA7Y/ref=oh_aui_detailpage_o08_s00?ie=UTF8&psc=1">
+        Joby Gorilla Pod Action Video Tripod
+      </Link> – 
+    </P>
+    <P>
+      <Link to="https://www.amazon.com/gp/product/B01HU3Q6S4/ref=oh_aui_detailpage_o03_s00?ie=UTF8&psc=1">
+        SanDisk Extreme 64GB microSDXC UHS-I Card with Adapter
+      </Link> – 
+    </P>
+
+    <H2>Software Products & Services</H2>
+    <P>
+      If you’re trying to set up a blog/website but tired of the default templates, going with a framework might be more desirable! Get a domain name with Namecheap, host it with Bluehost, and use Divi theme to customize our website!
+    </P>
+    <P>
+      <Link to="https://www.namecheap.com/">
+        Namecheap
+      </Link> – allows you to buy custom domain names which I prefer over GoDaddy. There seems to be less promotion and it’s much easier to use. There’s a dashboard to keep of all the domain names you purchase and reminds you when domains start expiring.
+    </P>
+    <P>
+      <Link to="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=33802">
+        Divi Theme by Elegant Themes
+      </Link> – is a modern solution that makes website building easy with building blocks and modules. With complete access to all themes including 87 at this point, extensive tutorial section, and unlimited website usage; it’s a phenomenal value for the price. I’ve created more than 3 websites with their product and love it. Coding knowledge is NOT required but you won’t be limited if you want to use CSS either. There’s split testing, visual builder, template saver, and many other functions. Play with their demo and see if it’s a good fit for you. Please use <Link to="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=33802">my affiliate link</Link> if you decide to get it!
+    </P>
+    <P>
+      <Link to="http://www.bluehost.com/track/timothyhoang/">
+        Bluehost
+      </Link> – is a popular company for good reason. It’s affordable, easy to use, and has excellent support. Compared to other hosting company, I’ve found Bluehost offers a great deal and excellent perks. I’ve already hosted six websites with them and they have not let me down yet. I would really recommend them for anyone wanting to start a new website. Consider a Plus plan if you plan on hosting many websites. Use <Link to="http://www.bluehost.com/track/timothyhoang/">my affiliate link</Link> if you decide to get it!
+    </P>
+    <P>
+      <Link to="https://www.lastpass.com/">
+        LastPass
+      </Link> – Can’t recommend this password management software enough.
+    </P>
+    <P>
+      <Link to="https://drive.google.com/settings/storage">
+        Google Drive 100GB
+      </Link> – For $2 a month, you can get 100GB so you can store all your important files on a cloud. This is a boon for digital nomads who don’t want to carry around an external hard drive. I keep my important documents, music, ebooks, and audiobooks on there. You can even download the desktop app to make your work flow even more fluid.
+    </P>
+
+    <H2>Web Development Resources</H2>
     <P>
       There are a million and one coding sites out there. These are some of the free ones I recommend you start with to see what kind of coding you like to learn. Take advantage of them and try out different resources to see what works for you!
     </P>
     <P>
-      <Link to="https://beta.freecodecamp.org/en/">Free Code Camp</Link> – Another great place to learn to code with free exercises and community. It’s considered a real school which you can list on LinkedIn and helps you get in contact with non-profits after you finish your certifications. However, many people get jobs way before they finish their certifications. Though a little vague on instruction sometimes, it’s still a great resource to get you started.
+      <Link to="#">Free Code Camp</Link> – Another great place to learn to code with free exercises and community. It’s considered a real school which you can list on LinkedIn and helps you get in contact with non-profits after you finish your certifications. However, many people get jobs way before they finish their certifications. Though a little vague on instruction sometimes, it’s still a great resource to get you started.
     </P>
     <P>
-      Codecademy – A very similar learning environment to Free Code Camp. Plenty of exercises and courses for you to sign up on. It’s free and really fun to learn. Instructions are much better but it does not have a large community to interact with others. The best way to learn is to read, apply, and see the results. This is a great place to do just that!
+      <Link to="https://www.codecademy.com/">Codecademy</Link> – A very similar learning environment to Free Code Camp. Plenty of exercises and courses for you to sign up on. It’s free and really fun to learn. Instructions are much better but it does not have a large community to interact with others. The best way to learn is to read, apply, and see the results. This is a great place to do just that!
+    </P>
+    <P>
+      <Link to="https://www.udemy.com/">Udemy</Link> – 
+    </P>
+    <P>
+      <Link to="https://www.udemy.com/">The Web Developer Bootcamp </Link> – My personal favorite course on Udemy. It’s taught by Colt Steele who has taught bootcamps in the past. I love the style of the course and how practical it was taught. Coding can be daunting for a beginner but the instructor knows about this and helps you feel like you’re making improvements every day. Please do not spend $200 on the course, Udemy has sales constantly and you can get this course for $10-15 easily.
+    </P>
+    <P>
+      <Link to="https://es6.io/">ES6 for Everyone </Link> – ES6 for Everyone is an awesome resource if you’re a JavaScript ninja. Get your chops down and learn all the updates that were released in 2015. Wes Bos is a great teacher and his videos are informative but do not leave you snoring. Wes covers template strings, promises, symbols, generators, proxies, and a ton more! Do yourself a favor if you doing JS, pick this up. Absolutely love this course and recommending to everyone I know!
+    </P>
+
+    <P>
+      Disclosure: Some of the links in this post are ‘affiliate links.’ This means if you click on the link and purchase the item, I will receive an affiliate commission. This is however at no extra cost to you. You’d just be helping me out 🙂
     </P>
   </Section>
 )
