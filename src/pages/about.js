b10846187b87abb454b26bc8908ccc0f9bf61c4d
Timothy Hoang <timh1203@gmail.com>
2018-06-25 16:13:18 -0400

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,14 +6,19 @@ const AboutPage = () => (
     <H1 modifiers={['my3']}>About</H1>
     <Img modifiers={['w75', 'frame']} src="https://i.imgur.com/NtmLXEU.jpg" alt="Profile Picture" />
     <P modifiers={['fLarge', 'w75', 'm0A']}>
-      I stepped away from a full-time job in the medical industry on October 28th, 2016. I left the corporate world behind and dedicated myself to redesigning my lifestyle so I can have more freedom in life. Although I have tried different ventures initially, I found that I love web development, which also allows for greater mobility and sustainability for a location-independent lifestyle. This is my journey in progress towards creating a life rich in experiences. I hope my experiences will inspire you to experience life intensely and seeking all the beauty encompassing our beautiful sphere.
+      In August 2015, I took a backpacking trip with a friend through Germany, Netherlands, Switzerland, and Italy. Little did I know that it would change my outlook on life for good. You see, it wasn't my first trip in Europe as I went previously in 2013 but there was something different this time. This time, a new friend and I talked about life over a bottle of wine in Florence, Italy. I asked him what's next for him after this, he said he doesn't know yet and will go where he wants. This is the same guy who was traveling in Montenegro a couple weeks before. I asked him how he would manage his life back home in Montana. He said he lives on the road now. My mind was blown. Shortly, my two week travel stint was over, it was time to head back stateside.
     <br />
     <br />
-      At the end of the day, we usually act in accordance with our estemeed values. Some important ones for me are time, location-freedom, health, and experiences. I want to take you with me and share how I'm building my lifestyle one brick a day to get closer to my goals. The very things we do every day define who we are and it's important not to forget about our dreams through fastidious, daily effort.
+      I came back to my Pharmacist job in a cold, windowless box curious about what's left out there to see. I remember driving in to work one night at 3AM in the morning during one of my on-call shifts. Yes, 3AM. Got home after 5AM and still had to work at 8AM the next day. Even though I knew I could do this only for a couple of years, I had already decided. I stepped away from a full-time job in the medical industry on October 28th, 2016. I left the corporate world behind and dedicated myself to redesigning my lifestyle so I can have more freedom in life. I crushed 90% of my medical student debt, researched about different online ventures, and focused with more clarity about my life than I ever did before.
     <br />
     <br />
-      Thanks for visiting my site!
+      It was time for change and I needed inspiration. I packed up my bags and decided to get lost to find myself. I flew down to Medellin, Colombia and was planning to stay only for a couple months. Three months turned into four, then five, then six. These were the times I tried different online ventures, met new friends, and temporarily freed myself from the 9-5 race. I finally gained perspective and found a love for web development, which allows for greater mobility and sustainability for a location-independent lifestyle. I decided to actively pursue this career track and come back to the states to find a web developer job.
     <br />
+    <br />
+      At the end of the day, we usually act in accordance with our estemeed values. Some important ones for me are time, location-freedom, health, and experiences. There's not one thing I can say that can change your values instantaneously, but instead I will live by example. The very things we do every day define who we are and I believe that discipline will ultimately set you free. Perhaps one day, you'll have an experience that shatters your reality. Until then, it's important not to forget about our dreams through fastidious, daily effort.
+    <br />
+    <br />
+      Thanks for visiting my site.
     <br />
       🌎Aho mitakuye oyasin💚
     </P>
