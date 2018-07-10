15a427deb7f0b9714d89bd2e9ac6ecc740ca256e
Timothy Hoang <timh1203@gmail.com>
2018-07-10 17:27:54 -0500

    Update tunes pics

--- src/components/PlaylistTunes.js
+++ src/components/PlaylistTunes.js
@@ -6,17 +6,17 @@ const VideoTunes = () => (
   <Wrapper id="videos">
     <H1>Coding Music</H1>
     <Div1>
-      <a href="https://www.youtube.com/watch?v=MsHPO4xdwKg&list=PLPTS2VjPOpjVER26b8BstdcPgfO4qjYpn" rel="noopener noreferrer" target="_blank">
-        <Img1 src="https://i.imgur.com/pF25HFj.png" alt="Endurance Coding Music Playlist" />
+      <a href="https://www.youtube.com/watch?v=244AT-9e-cU&list=PLPTS2VjPOpjUBnIFFUphcKqzdBLTWPfqQ" rel="noopener noreferrer" target="_blank">
+        <Img1 src="https://i.imgur.com/4YMiSRr.png" alt="Ambition Coding Music Playlist" />
       </a>
       <a href="https://www.youtube.com/watch?v=HEf_xrgmuRI&list=PLPTS2VjPOpjWXnLm0Qsc-K0YkkYnK88PH" rel="noopener noreferrer" target="_blank">
-        <Img1 src="https://i.imgur.com/okH1YXk.png" alt="Fortitude Coding Music Playlist" />
+        <Img1 src="https://i.imgur.com/Prgiw5l.png" alt="Honor Coding Music Playlist" />
+      </a>
+      <a href="https://www.youtube.com/watch?v=MsHPO4xdwKg&list=PLPTS2VjPOpjVER26b8BstdcPgfO4qjYpn" rel="noopener noreferrer" target="_blank">
+        <Img1 src="https://i.imgur.com/MozKUmq.png" alt="Endurance Coding Music Playlist" />
       </a>
       <a href="https://www.youtube.com/watch?v=94TAFSMdkvk&list=PLPTS2VjPOpjUSTmVc9Y4MigDP-q_MZ6KH" rel="noopener noreferrer" target="_blank">
-        <Img1 src="https://i.imgur.com/APOM2jJ.png" alt="Valor Coding Music Playlist" />
-      </a>
-      <a href="https://www.youtube.com/watch?v=244AT-9e-cU&list=PLPTS2VjPOpjUBnIFFUphcKqzdBLTWPfqQ" rel="noopener noreferrer" target="_blank">
-        <Img1 src="https://i.imgur.com/XlUtSxO.png" alt="Ambition Coding Music Playlist" />
+        <Img1 src="https://i.imgur.com/aLuyXMp.png" alt="Valor Coding Music Playlist" />
       </a>
     </Div1>
   </Wrapper>
